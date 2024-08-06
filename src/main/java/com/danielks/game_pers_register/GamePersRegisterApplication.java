9c579af701742996ec2be836efeb38eb38a733c0
dschifel <danielschifelbain@gmail.com>
2024-08-06 20:13:37 -0300

    Created project

---
+++ src/main/java/com/danielks/game_pers_register/GamePersRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.danielks.game_pers_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamePersRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamePersRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielks/game_pers_register/GamePersRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielks.game_pers_register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamePersRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

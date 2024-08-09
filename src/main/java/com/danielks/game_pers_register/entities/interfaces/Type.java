d57c8bb44f8845779ea41fa9ffb9e2cd57cc86a8
dschifel <danielschifelbain@gmail.com>
2024-08-09 18:03:33 -0300

    Removed interface type

--- src/main/java/com/danielks/game_pers_register/entities/interfaces/Type.java
+++
@@ -1,7 +0,0 @@
-package com.danielks.game_pers_register.entities.interfaces;
-
-public interface Type {
-    int getPhysicalStrength();
-    int getRangedStrength();
-    String getCombatStance();
-}

1a70ffa7f6377a0bec6bac67ad578bbbf1f72cb4
dschifel <danielschifelbain@gmail.com>
2024-08-10 17:20:26 -0300

    Created data.sql to start tables + created inserts for the skills

---
+++ src/main/resources/data.sql
@@ -0,0 +1,42 @@
+-- SKILLS SECTION
+
+-- wizard skills
+-- active wizard skill
+INSERT INTO active_skill (id, name, skill_value) VALUES
+('f47ac10b-58cc-4372-a567-0e02b2c3d479', 'Teleport', 5);
+
+-- passive wizard skill
+INSERT INTO passive_skill (id, name, skill_value) VALUES
+('dce0f0d8-8f54-4d8a-bbe2-7c35a8ea2b3e', 'Magic Attack', 5);
+
+-- magical wizard skill
+INSERT INTO magic_skill (id, name, skill_value) VALUES
+('a43e45b4-9a1b-4efc-b6d7-5c2b9e64f61a', 'Fireball', 5);
+
+--
+-- warrior skills
+-- active warrior skill
+INSERT INTO active_skill (id, name, skill_value) VALUES
+('e16b18a7-f3b4-462e-b77f-d799e1e8b8e7', 'Critical Attack', 5);
+
+-- passive warrior skill
+INSERT INTO passive_skill (id, name, skill_value) VALUES
+('7d16d26b-5d2b-42b8-91f0-3b45a68cbb6b', 'Sword Attack', 5);
+
+-- magical warrior skill
+INSERT INTO magic_skill (id, name, skill_value) VALUES
+('b9a7641e-070d-4bfc-b6a2-98b44c2d78e6', 'Area Attack', 5);
+
+--
+-- long-distance skills
+-- active long-distance skill
+INSERT INTO active_skill (id, name, skill_value) VALUES
+('24bdf5d4-6b67-47af-8732-03b29f3b4cf3', 'Explosive Arrow', 5);
+
+-- passive long-distance skill
+INSERT INTO passive_skill (id, name, skill_value) VALUES
+('cc5b6b1c-8d72-4aef-9a23-3b26ef21b11e', 'Arrow Attack', 5);
+
+-- magical long-distance skill
+INSERT INTO magic_skill (id, name, skill_value) VALUES
+('f7c20e8a-b4f4-4df1-888e-0c2d2a1f9fd6', 'Grenade Attack', 5);
